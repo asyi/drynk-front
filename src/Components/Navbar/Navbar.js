512b86019b5800626d16e72dd2e04ee577ad0ec3
althea <altheaserena.yi@gmail.com>
2018-11-24 20:04:55 -0500

    refactors navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Navbar.css';
+
 import filter from '../../images/filter.png';
+import logo from '../../images/logo.png';
 
 function Navbar() {
   return (
-    <div className="navbarTitleWrapper borderBottom">
-      <h1 className="navTitle">Drynk</h1>
-      <img className="navFilter" alt="Filter icon" src={filter} />
+    <div className="navbkg">
+      <div className="navbarTitleWrapper borderBottom">
+        <img className="navFilter" alt="Filter icon" src={filter} />
+        <img className="navLogo" alt="Drynk Logo" src={logo} />
+      </div>
     </div>
   );
 }
